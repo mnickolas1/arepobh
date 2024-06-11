ad542401b8e051e8eb213920b4d2a086b223dd4b
Nick <mnickolas.1@gmail.com>
2024-06-11 20:08:03 +1000

    add mass without thermal energy

--- src/blackholes/bh_update.c
+++ src/blackholes/bh_update.c
@@ -269,7 +269,7 @@ void virtual_part_feedback(void)
 /*-> we need to do this here so that particle properties don't change between loading the buffer and emptying it*/
 
       /*update total energy*/
-      SphP[i].Energy += SphP[i].FMass*SphP[i].Utherm + SphP[i].Fkin;
+      SphP[i].Energy += /*SphP[i].FMass*SphP[i].Utherm +*/ SphP[i].Fkin;
 
       kick_vector[0] = SphP[i].FMomentum[0];
       kick_vector[1] = SphP[i].FMomentum[1];
@@ -287,7 +287,7 @@ void virtual_part_feedback(void)
           
       pj = -p0*cos_theta + sqrt(p0*p0 * cos_theta*cos_theta + 2*P[i].Mass*SphP[i].Fkin);*/
 
-      pj = sqrt(2 * P[i].Mass * (SphP[i].Energy - P[i].Mass * SphP[i].Utherm)) - p0;
+      pj = sqrt(2 * P[i].Mass * (SphP[i].Energy - (P[i].Mass-SphP[i].FMass)*SphP[i].Utherm /*P[i].Mass * SphP[i].Utherm*/)) - p0;
 
       bh_momentum_kick[0] = kick_vector[0] * pj / sqrt(pow(kick_vector[0], 2) + pow(kick_vector[1], 2) + pow(kick_vector[2], 2));
       bh_momentum_kick[1] = kick_vector[1] * pj / sqrt(pow(kick_vector[0], 2) + pow(kick_vector[1], 2) + pow(kick_vector[2], 2));
