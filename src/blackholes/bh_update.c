0e3a19f13573c3d292b76a8407b90ac2108ad4c9
Nick <mnickolas.1@gmail.com>
2024-07-16 15:57:27 +0800

    Update bh_update.c

--- src/blackholes/bh_update.c
+++ src/blackholes/bh_update.c
@@ -271,7 +271,7 @@ void virtual_part_feedback(void)
       double u   = pow(10, 7) * BOLTZMANN / GAMMA_MINUS1 / PROTONMASS / 0.6 / 
         (All.UnitEnergy_in_cgs/All.UnitMass_in_g);
       /*update total energy*/
-      SphP[i].Energy += SphP[i].FMass*u + SphP[i].Fkin;
+      SphP[i].Energy += /*SphP[i].FMass*u +*/ SphP[i].Fkin;
 
       kick_vector[0] = SphP[i].FMomentum[0];
       kick_vector[1] = SphP[i].FMomentum[1];
@@ -289,7 +289,7 @@ void virtual_part_feedback(void)
           
       pj = -p0*cos_theta + sqrt(p0*p0 * cos_theta*cos_theta + 2*P[i].Mass*SphP[i].Fkin);*/
 
-      pj = sqrt(2 * P[i].Mass * (SphP[i].Energy - (P[i].Mass-SphP[i].FMass)*SphP[i].Utherm - SphP[i].FMass * u)) - p0;
+      pj = sqrt(2 * P[i].Mass * (SphP[i].Energy - (P[i].Mass-SphP[i].FMass)*SphP[i].Utherm /*- SphP[i].FMass * u*/)) - p0;
 
       bh_momentum_kick[0] = kick_vector[0] * pj / sqrt(pow(kick_vector[0], 2) + pow(kick_vector[1], 2) + pow(kick_vector[2], 2));
       bh_momentum_kick[1] = kick_vector[1] * pj / sqrt(pow(kick_vector[0], 2) + pow(kick_vector[1], 2) + pow(kick_vector[2], 2));
