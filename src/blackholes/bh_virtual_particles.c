a1d6a751395da2e000b608d6fb7b03c9dc7c3e54
Nick <mnickolas.1@gmail.com>
2024-07-22 20:28:51 +0800

    Better seed for srand

--- src/blackholes/bh_virtual_particles.c
+++ src/blackholes/bh_virtual_particles.c
@@ -11,6 +11,20 @@
 
 #define DEG_TO_RAD(deg) ((deg) * M_PI / 180.0)
 
+// Robert Jenkins' 96 bit Mix Function
+unsigned long mix(unsigned long a, unsigned long b, unsigned long c)
+{
+    a=a-b;  a=a-c;  a=a^(c >> 13);
+    b=b-c;  b=b-a;  b=b^(a << 8);
+    c=c-a;  c=c-b;  c=c^(b >> 13);
+    a=a-b;  a=a-c;  a=a^(c >> 12);
+    b=b-c;  b=b-a;  b=b^(a << 16);
+    c=c-a;  c=c-b;  c=c^(b >> 5);
+    a=a-b;  a=a-c;  a=a^(c >> 3);
+    b=b-c;  b=b-a;  b=b^(a << 10);
+    c=c-a;  c=c-b;  c=c^(b >> 15);
+    return c;
+}
 
 void create_particles(void)
 {
@@ -58,8 +72,9 @@ void create_particles(void)
 
           newid++;
 
-/* Assign properties to the spawned particles */       
-          srand(time(NULL));
+/* Assign properties to the spawned particles */ 
+          unsigned long seed = mix(clock(), time(NULL), getpid());
+          srand(seed);
 
           double phi, theta;
 
