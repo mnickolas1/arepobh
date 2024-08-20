b08c24de3ba760c48b2cbf2a73b27584fd87c090
Nick <mnickolas.1@gmail.com>
2024-08-20 10:57:27 +0100

    0-> i

--- src/blackholes/bh_density.c
+++ src/blackholes/bh_density.c
@@ -254,7 +254,7 @@ void bh_density(void)
           if(iter > 0) 
             { 
               for(i=0; i<NumBh; i++)
-                printf("BH_DENSITY: ngb iteration %d:-> Hsml: %f, NumNgb: %f, Nmass: %f\n", iter, BhP[0].Hsml, BhNumNgb[0], BhP[0].NgbMass);
+                printf("BH_DENSITY: ngb iteration %d:-> Hsml: %f, NumNgb: %f, Nmass: %f\n", iter, BhP[i].Hsml, BhNumNgb[i], BhP[i].NgbMass);
             }
           if(iter > 48)
             terminate("failed to converge in neighbour iteration in bh_density()\n");
